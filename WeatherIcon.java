1240abfb4bf8de8ed28b5f011a5d1120de7c4fbc
Lucas Davey <l14dave@siena.edu>
2026-03-30 18:27:00 -0400

    Started WeatherIcon class

---
+++ WeatherIcon.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * This program will display the weather icons such as
+ * sunny, partly cloudy, cloudy, light rain, moderate/heavy rain, and wind
+ * 
+ * 
+ * @author Lucas Davey
+ * @version Spring 2026
+ */
+
+public abstract class WeatherIcon {
+    
+
+}
