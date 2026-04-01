48329844db4b3c935c7250c1893f4c57b70ff49c
Patrick-McFee <p21mcfe@siena.edu>
2026-03-31 20:03:06 -0400

    more changes

--- WeatherData.java
+++ WeatherData.java
@@ -1,38 +1,34 @@
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 
 /**
- * Interface for retrieving atmospheric data from remote weather services.
+ * Interface for retrieving weather data
  * @author Patrick McFee
  * @version Spring 2026
  */
 public interface WeatherData {
     
     /**
-     * Retrieves the temperature at a specific time and location.
-     * @param dateTime The specific date and time for the forecast.
-     * @param zipCode  The postal code for the location.
-     * @return Temperature in Fahrenheit.
+     * Retrieves the temperature
+     * @return An ArrayList of tempatures thoughout the day
      */
-    double getTemperature(ZonedDateTime dateTime);
+    ArrayList<Double> getTemperature();
 
     /**
-     * Retrieves the type of precipitation (e.g., "Rain", "Snow", "None").
-     * @param dateTime The specific date and time.
+     * Retrieves the type of precipitation
      * @return A string representing the precipitation type.
      */
-    String getPrecipitationType(ZonedDateTime dateTime);
+    String getPrecipitationType();
 
     /**
      * Retrieves the wind speed.
-     * @param dateTime The specific date and time.
      * @return Wind speed in miles per hour.
      */
-    double getWindSpeed(ZonedDateTime dateTime);
+    double getWindSpeed();
 
     /**
      * Retrieves the probability of precipitation.
-     * @param dateTime The specific date and time.
      * @return Percentage chance (0-100).
      */
-    int getPrecipitationProbability(ZonedDateTime dateTime);
+    int getPrecipitationProbability();
 }
