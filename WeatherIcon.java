f06a81e1ca7a407606ef441c1920ff4c8c59480b
Lucas Davey <l14dave@siena.edu>
2026-04-01 12:27:51 -0400

    added constants for weather icon colors

--- WeatherIcon.java
+++ WeatherIcon.java
@@ -1,4 +1,5 @@
-
+import java.awt.Color;
+import javax.swing.JPanel;
 
 /**
  * This program will display the weather icons such as
@@ -9,7 +10,21 @@
  * @version Spring 2026
  */
 
-public abstract class WeatherIcon {
-    
+public class WeatherIcon {
+    private JPanel panel;
+    private String weatherCondition;
+
+    //cloud colors
+    private final Color CLOUD_WHITE = new Color(230,230,240);
+    private final Color CLOUD_GRAY = new Color(140,140,160);
+    private final Color CLOUD_DARK = new Color(70,70,90);
+
+    //rain and lightning colors
+    private final Color RAIN_BLUE = new Color(80,140,200);
+    private final Color LIGHTNING_BOLT = new Color(255,240,60);
+
+
+
+   
 
 }
