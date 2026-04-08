9a03827aedb6019c30931997b900e9ed6c2e6e3b
Lucas Davey <l14dave@siena.edu>
2026-04-07 20:49:18 -0400

    sunnyDay() method works. will make it look better soon

--- WeatherIcon.java
+++ WeatherIcon.java
@@ -1,74 +1,90 @@
+
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 /**
- * This program will display the weather icons such as
- * sunny, partly cloudy, cloudy, light rain, moderate/heavy rain, and wind
- * 
- * 
+ * This program will display the weather icons such as sunny, partly cloudy,
+ * cloudy, light rain, moderate/heavy rain, and wind
+ *
+ *
  * @author Lucas Davey
  * @version Spring 2026
  */
+public class WeatherIcon extends JPanel implements Runnable {
 
-public class WeatherIcon implements Runnable {
-    private JPanel panel;
     private String weatherCondition;
 
     //cloud colors
-    private final Color CLOUD_WHITE = new Color(230,230,240);
-    private final Color CLOUD_GRAY = new Color(140,140,160);
-    private final Color CLOUD_DARK = new Color(70,70,90);
+    private final Color CLOUD_WHITE = new Color(230, 230, 240);
+    private final Color CLOUD_GRAY = new Color(140, 140, 160);
+    private final Color CLOUD_DARK = new Color(70, 70, 90);
 
     //rain and lightning colors
-    private final Color RAIN_BLUE = new Color(80,140,200);
-    private final Color LIGHTNING_BOLT = new Color(255,240,60);
+    private final Color RAIN_BLUE = new Color(80, 140, 200);
+    private final Color LIGHTNING_BOLT = new Color(255, 240, 60);
 
-    public WeatherIcon(String condition){
+    public WeatherIcon(String condition) {
         this.weatherCondition = condition;
     }
 
     @Override
-    public void run(){
+    public void run() {
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("Weather: " + weatherCondition);
         frame.setPreferredSize(new Dimension(300, 300));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.add(panel);
+        frame.add(this);
         frame.pack();
         frame.setVisible(true);
     }
 
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
-    private void sunnyDay(){
-
-    }
-
-    private void partlyCloudy(){
+        sunnyDay(g);
 
     }
 
-    private void cloudyDay(){
+    private void sunnyDay(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillOval(100, 100, 80, 80);
+
+        g.setColor(Color.ORANGE);
+        g.drawLine(140, 100, 140, 60); //top
+        g.drawLine(180, 140, 220, 140); //right
+        g.drawLine(140, 180, 140, 220); //bottom
+        g.drawLine(100, 140, 60, 140);  //left
 
     }
 
-    private void rainyDay(){
+    private void partlyCloudy() {
 
     }
 
-    private void thunderstorm(){
+    private void cloudyDay() {
 
     }
 
-    private void windyDay(){
+    private void rainyDay() {
 
     }
 
-    public static void main(String args[]){
-        
+    private void thunderstorm() {
+
     }
-   
+
+    private void windyDay() {
+
+    }
+
+    public static void main(String args[]) {
+        SwingUtilities.invokeLater(new WeatherIcon("sunny"));
+
+    }
 
 }
